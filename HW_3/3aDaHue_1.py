b5280c90cfe66a7c3a4cffc06b6a47903a498b6d
Kolomets Denis <denkolomets@mail.ru>
2022-10-19 02:32:22 +0300

    New branch completed successfully

--- HW_3/3aDaHue_1.py
+++ HW_3/3aDaHue_1.py
@@ -2,9 +2,8 @@ N = int(input('Введите число элементов массива: '))
 from random import randint
 rand_numbers = []
 sum_numbers = 0
-for i in range(0, N):
+for i in range(1, N, 2):
     rand_numbers.append(randint(0, 20))
-    if i % 2 == 0:
-        sum_numbers = sum_numbers + int(rand_numbers[i])
+    sum_numbers += int(rand_numbers[i])
 print(rand_numbers)
 print(sum_numbers)
