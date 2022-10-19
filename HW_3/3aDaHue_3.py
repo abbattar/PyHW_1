c7cc6d61242ffd5ac19d90677e2a56dcbf8da32b
Kolomets Denis <denkolomets@mail.ru>
2022-10-19 08:32:56 +0300

    noup

--- HW_3/3aDaHue_3.py
+++ HW_3/3aDaHue_3.py
@@ -5,7 +5,7 @@ for i in range(0, N):
     count_str = float(input('Введите значение элемента: '))
     count_list.append(count_str)
     count_list_str.append(count_list[i] % 1)
-    max_count, min_count = count_list_str[0]
+    max_count, min_count = count_list_str[0], count_list_str[0]
     for j in count_list_str:
         if float(j) > max_count:
             max_count = j
