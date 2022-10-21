63937bbd6e7b6137c564baa553b26a179cd190da
Kolomets Denis <denkolomets@mail.ru>
2022-10-21 15:41:30 +0300

    Задание 2 решено

--- HW_4/3aDaHue_1.py
+++ HW_4/3aDaHue_1.py
@@ -2,15 +2,14 @@ x = float(input('Введите некоторое число: '))
 d_pow = int(input('Введите степень округления (не более 10): '))
 d = 1 / (10 ** d_pow)
 def round_x(x, d_pow):
-    y = (x - x % 1) + (((x % 1 ) * 10 ** d_pow) - ((x % 1 ) * 10 ** d_pow) % 1) * (1 / (10 ** d_pow))
+    y = (x - x % 1) + (((x % 1 ) * 10 ** d_pow) 
+                       - ((x % 1 ) * 10 ** d_pow) % 1) * ( 1 / (10 ** d_pow))
     return y
-str_decimal_x = str(round_x(x, d_pow)).split('.')[1]
 str_int_x = str(round_x(x, d_pow)).split('.')[0]
+str_decimal_x = str(round_x(x, d_pow)).split('.')[1]
 len_x = len(str_int_x) + len(str_decimal_x[:d_pow + 1]) + 1     # + 1 из-за присутствия точки
-print(len_x)
-print(str(d))
+# print(len_x)
 print(str(round_x(x, d_pow + 1)).split('.')[1][d_pow:d_pow + 1])
 print(str(round_x(x, d_pow))[:len_x] if 
       int(str(round_x(x, d_pow + 1)).split('.')[1][d_pow:d_pow + 1]) < 5
      else str(round_x(x, d_pow) + d)[:len_x])
-#if int(str(round_x)[d_pow::d_pow+1] )
