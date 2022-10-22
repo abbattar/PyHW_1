fe434f48e8238b25b26c80d251aeffa7251427f6
Kolomets Denis <denkolomets@mail.ru>
2022-10-22 13:56:11 +0300

    задание 4 готово

--- HW_4/3aDaHue_4.py
+++ HW_4/3aDaHue_4.py
@@ -2,17 +2,18 @@ from random import randint
 k = int(input('Введите степень многочлена: '))
 
 str_polinome = []
-for i in range(k+1):
+for i in range(0,k+1):
     str_polinome.append(randint(0,100))
 if str_polinome[k] == 0:
-    print('По независящим от нас причинам произошла ошибка', str_polinome[0])
+    print('По независящим от нас причинам произошла ошибка', str_polinome[k])
     exit()
 else:
     # path = './HW_4/polinome.txt'
     with open(r"./HW_4/polinome.txt", 'w') as file:
         str_line =[]
         str_line.append(str(str_polinome[0]))
-        for i in range(1, k+1):
-            str_line.append( f' * {i}^x')
+        for i in range(1,k+1):
+            str_line.append( f' * x^{i}')
             str_line[i] = str(str_polinome[i]) + str_line[i]
-        file.write("Polinome = "+" + ".join(str_line)[::-1])
+        file.write("Polinome = "+" + ".join(str_line))  # Если перевернуть строку,
+                                                        # то число ху будет ух
