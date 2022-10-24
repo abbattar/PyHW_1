142c1f59bd9e8534b1c19006992d6c007e88617a
Kolomets Denis <denkolomets@mail.ru>
2022-10-24 19:04:14 +0300

    ***

--- HW_4/3aDaHue_2.py
+++ HW_4/3aDaHue_2.py
@@ -5,23 +5,26 @@ n = int(input('Введите какое-то натуральное число 
 
 # Решето Эратосфена для нахождения простых чисел
 
+
 def primes(n):
     sieve = [True] * n
     for i in range(3, int(n**0.5)+1, 2):
         if sieve[i]:
-            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
-    return [2] + [i for i in range(3,n,2) if sieve[i]]
+            sieve[i*i::2*i] = [False]*((n-i*i-1)//(2*i)+1)
+    return [2] + [i for i in range(3, n, 2) if sieve[i]]
+
+
 print(primes(n))
 
 # 2 примера 1-й из урока, второй подсмотрел в интернете до урока
-prime =[]
+prime = []
 for i in primes(n):
     prime = primes(i)   # работает без append O_o
 res = []
 tmp = n
 i = 0
 while tmp > 1:
-    if not(tmp % int(prime[i])):
+    if not (tmp % int(prime[i])):
         tmp //= prime[i]
         res.append(prime[i])
     else:
@@ -32,9 +35,9 @@ m = n
 factors = []
 d = 2
 while d*d <= n:
-    if not(n % d):
+    if not (n % d):
         factors.append(d)
-        n//=d
+        n //= d
     else:
         d += 1
 factors.append(n)
--- HW_4/3aDaHue_3.py
+++ HW_4/3aDaHue_3.py
@@ -4,12 +4,13 @@ lis_t = []
 for i in range(0, length_list):
     lis_t.append(randint(0, 20))
 print(lis_t)
-lis_t_filt = list(set(lis_t))   # Способ, при котором не только удаляются все 
-                                # одинаковые элементы списка, но и меняется их 
-                                # последовательность => от меньшего к большему
+lis_t_filt = list(set(lis_t))   # Способ, при котором не только удаляются все
+# одинаковые элементы списка, но и меняется их
+# последовательность => от меньшего к большему
 print(lis_t_filt)
 
 # 2 cпособа при которых не теряется сортировка исходного списка
+
 
 def f(li_st):
     n = []
@@ -17,7 +18,10 @@ def f(li_st):
         if i not in n:
             n.append(i)
     return n
+
+
 print(f(lis_t))
+
 
 def unique(obj: iter):
     args = []
@@ -25,6 +29,7 @@ def unique(obj: iter):
         if a not in args:
             args.append(a)
             yield a
+
 
 r = unique(lis_t)
 print([*r])
